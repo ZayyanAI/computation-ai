a7aacd898ab6f5bcccfd0962f8e265232b2453e5
ZayyanAI <zayyan4ali@gmail.com>
2026-09-18 19:36:11 +0700

    .

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://computation-ai.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
